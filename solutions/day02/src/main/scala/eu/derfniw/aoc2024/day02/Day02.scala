ccb5c4be64b7947686d4cd52024fd5f6f9692456
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-01 08:06:18 +0100

    Day 2 scaffold

---
+++ solutions/day02/src/main/scala/eu/derfniw/aoc2024/day02/Day02.scala
@@ -0,0 +1,11 @@
+package eu.derfniw.aoc2024.day02
+
+import eu.derfniw.aoc2024.util.InputReader
+
+
+object Inputs extends InputReader(2)
+
+
+@main
+def day02(): Unit =
+  println("TODO!")
---
+++ solutions/day02/src/test/scala/eu/derfniw/aoc2024/day02/Day02Test.scala
@@ -0,0 +1,12 @@
+package eu.derfniw.aoc2024.day02
+
+import eu.derfniw.aoc2024.util.InputReader
+
+class Day02Test extends munit.FunSuite, InputReader(2):
+  lazy val input1 = readInputFile("test1.txt").get
+
+  test("part1") {
+    // TODO!
+  }
+
+end Day02Test
