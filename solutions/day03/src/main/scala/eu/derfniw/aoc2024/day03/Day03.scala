c97843a740c83957a5c86f28d19055db8c5a7629
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-02 08:12:52 +0100

    Day 3 scaffold.

---
+++ solutions/day03/src/main/scala/eu/derfniw/aoc2024/day03/Day03.scala
@@ -0,0 +1,14 @@
+package eu.derfniw.aoc2024.day03
+
+import eu.derfniw.aoc2024.util.InputReader
+
+def part1(input: Seq[String]): Int = ???
+
+def part2(input: Seq[String]): Int = ???
+
+object Inputs extends InputReader(3)
+
+@main
+def day03(): Unit =
+  // TODO
+  ()
