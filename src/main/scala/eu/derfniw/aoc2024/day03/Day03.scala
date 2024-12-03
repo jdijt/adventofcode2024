101cdddae3b8dc7c86e0daa504d09d9ec3078c8f
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-03 09:20:42 +0100

    Day3 implementation

--- src/main/scala/eu/derfniw/aoc2024/day03/Day03.scala
+++ src/main/scala/eu/derfniw/aoc2024/day03/Day03.scala
@@ -2,13 +2,46 @@ package eu.derfniw.aoc2024.day03
 
 import eu.derfniw.aoc2024.util.InputReader
 
-def part1(input: Seq[String]): Int = ???
+enum Instruction:
+  case Mul(a: Int, b: Int)
+  case Do
+  case Dont
 
-def part2(input: Seq[String]): Int = ???
+case class ComputingState(acc: Int, mulEnabled: Boolean):
+  def apply(instr: Instruction): ComputingState = instr match
+    case Instruction.Mul(a, b) if this.mulEnabled => this.copy(acc = this.acc + (a * b))
+    case Instruction.Do                           => this.copy(mulEnabled = true)
+    case Instruction.Dont                         => this.copy(mulEnabled = false)
+    case _                                        => this
+
+object ComputingState:
+  def initial: ComputingState = ComputingState(0, true)
+
+def part1(input: Seq[String]): Int =
+  val fullInput = input.mkString("\n")
+  val mulRegex  = """mul\((\d+?),(\d+?)\)""".r
+  mulRegex
+    .findAllMatchIn(fullInput)
+    .map { case mulRegex(a, b) => Instruction.Mul(a.toInt, b.toInt) }
+    .foldLeft(ComputingState.initial)(_.apply(_))
+    .acc
+
+def part2(input: Seq[String]): Int =
+  val fullInput  = input.mkString("\n")
+  val instrRegex = """(mul\(\d+?,\d+?\)|do\(\)|don't\(\))""".r
+  instrRegex
+    .findAllMatchIn(fullInput)
+    .map {
+      case instrRegex(s"mul($a,$b)") => Instruction.Mul(a.toInt, b.toInt)
+      case instrRegex("do()")        => Instruction.Do
+      case instrRegex("don't()")     => Instruction.Dont
+    }
+    .foldLeft(ComputingState.initial)(_.apply(_))
+    .acc
 
 object Inputs extends InputReader(3)
 
 @main
 def day03(): Unit =
-  // TODO
-  ()
+  println(s"Part 1: ${part1(Inputs.mainInput)}")
+  println(s"Part 2: ${part2(Inputs.mainInput)}")
