8fb0da9231a14df5f8017e40bb72becb6488bee1
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-11 12:46:39 +0100

    Day 11 - Memoized & fast.

--- src/main/scala/eu/derfniw/aoc2024/day11/Day11.scala
+++ src/main/scala/eu/derfniw/aoc2024/day11/Day11.scala
@@ -2,30 +2,40 @@ package eu.derfniw.aoc2024.day11
 
 import eu.derfniw.aoc2024.util.{InputReader, runBenchmarked}
 
-class StoneRow(val row: List[Long]):
-  def blink(): StoneRow =
-    val newRow = row.flatMap {
-      case 0 => List(1L)
-      case n if n.toString.length % 2 == 0 =>
-        val (left, right) = n.toString.splitAt(n.toString.length / 2)
-        List(left.toLong, right.toLong)
-      case other => List(other * 2024L)
-    }
-    StoneRow(newRow)
-end StoneRow
+type StoneRow = List[Int]
 
-object StoneRow:
-  def fromInput(s: Seq[String]): StoneRow = StoneRow(s.head.split(" ").map(_.toLong).toList)
+def calculateLengthAfterBlinks(stoneRow: StoneRow, blinks: Int): Long =
+  val memory = collection.mutable.Map.empty[(Int, Long), Long]
+  def helper(stone: Long, blinksLeft: Int): Long =
+    if memory.contains((blinksLeft, stone)) then memory((blinksLeft, stone))
+    else if blinksLeft == 0 then 1
+    else
+      val result = stone match
+        case 0 => helper(1, blinksLeft - 1)
+        case n if n.toString.length % 2 == 0 =>
+          val nStr          = n.toString
+          val (left, right) = nStr.splitAt(nStr.length / 2)
+          helper(left.toLong, blinksLeft - 1) + helper(right.toLong, blinksLeft - 1)
+        case other => helper(other * 2024, blinksLeft - 1)
+      memory((blinksLeft, stone)) = result
+      result
 
-def part1(input: Seq[String]): Int =
-  (1 to 25).foldLeft(StoneRow.fromInput(input))((acc, _) => acc.blink()).row.size
+  stoneRow.map(helper(_, blinks)).sum
+end calculateLengthAfterBlinks
 
-def part2(input: Seq[String]): Int =
-  (1 to 75).foldLeft(StoneRow.fromInput(input))((acc, _) => acc.blink()).row.size
+def parseInput(s: Seq[String]): StoneRow = s.head.split(" ").map(_.toInt).toList
+
+def part1(input: Seq[String]): Long =
+  val stoneRow = parseInput(input)
+  calculateLengthAfterBlinks(stoneRow, 25)
+
+def part2(input: Seq[String]): Long =
+  val stoneRow = parseInput(input)
+  calculateLengthAfterBlinks(stoneRow, 75)
 
 object Inputs extends InputReader(11)
 
 @main
 def day11(): Unit =
   println(s"Part1:\n ${runBenchmarked(Inputs.mainInput, part1).pretty}")
-  println(s"Part1:\n Result: ${part2(Inputs.mainInput)}")
+  println(s"Part2:\n ${runBenchmarked(Inputs.mainInput, part2).pretty}")
