85a5409dbfcd263e2cdc1459fbc37b36e2db5456
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-11 16:02:30 +0100

    Day 11 - More classy solution ;) .

--- src/main/scala/eu/derfniw/aoc2024/day11/Day11.scala
+++ src/main/scala/eu/derfniw/aoc2024/day11/Day11.scala
@@ -2,36 +2,36 @@ package eu.derfniw.aoc2024.day11
 
 import eu.derfniw.aoc2024.util.{InputReader, runBenchmarked}
 
-type StoneRow = List[Int]
+class StoneRow(row: List[Int]):
+  def calculateLengthAfterBlinks(blinks: Int): Long =
+    val memory = collection.mutable.Map.empty[(Int, Long), Long]
+    def helper(stone: Long, blinks: Int): Long =
+      if memory.contains((blinks, stone)) then memory((blinks, stone))
+      else if blinks == 0 then 1
+      else
+        val result = stone match
+          case 0 => helper(1, blinks - 1)
+          case n if n.toString.length % 2 == 0 =>
+            val nStr          = n.toString
+            val (left, right) = nStr.splitAt(nStr.length / 2)
+            helper(left.toLong, blinks - 1) + helper(right.toLong, blinks - 1)
+          case other => helper(other * 2024, blinks - 1)
+        memory((blinks, stone)) = result
+        result
+    row.map(helper(_, blinks)).sum
+  end calculateLengthAfterBlinks
+end StoneRow
 
-def calculateLengthAfterBlinks(stoneRow: StoneRow, blinks: Int): Long =
-  val memory = collection.mutable.Map.empty[(Int, Long), Long]
-  def helper(stone: Long, blinksLeft: Int): Long =
-    if memory.contains((blinksLeft, stone)) then memory((blinksLeft, stone))
-    else if blinksLeft == 0 then 1
-    else
-      val result = stone match
-        case 0 => helper(1, blinksLeft - 1)
-        case n if n.toString.length % 2 == 0 =>
-          val nStr          = n.toString
-          val (left, right) = nStr.splitAt(nStr.length / 2)
-          helper(left.toLong, blinksLeft - 1) + helper(right.toLong, blinksLeft - 1)
-        case other => helper(other * 2024, blinksLeft - 1)
-      memory((blinksLeft, stone)) = result
-      result
-
-  stoneRow.map(helper(_, blinks)).sum
-end calculateLengthAfterBlinks
-
-def parseInput(s: Seq[String]): StoneRow = s.head.split(" ").map(_.toInt).toList
+object StoneRow:
+  def fromInput(input: Seq[String]): StoneRow = StoneRow(input.head.split(" ").map(_.toInt).toList)
 
 def part1(input: Seq[String]): Long =
-  val stoneRow = parseInput(input)
-  calculateLengthAfterBlinks(stoneRow, 25)
+  val stoneRow = StoneRow.fromInput(input)
+  stoneRow.calculateLengthAfterBlinks(25)
 
 def part2(input: Seq[String]): Long =
-  val stoneRow = parseInput(input)
-  calculateLengthAfterBlinks(stoneRow, 75)
+  val stoneRow = StoneRow.fromInput(input)
+  stoneRow.calculateLengthAfterBlinks(75)
 
 object Inputs extends InputReader(11)
 
