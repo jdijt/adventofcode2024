ecbe753d92a73f6cacfac2bbbd90bd207a1c95ed
Jasper Dijt <jasper@jasperdijt.eu>
2024-12-22 16:52:21 +0100

    Day 21 done.

--- src/main/scala/eu/derfniw/aoc2024/day21/Day21.scala
+++ src/main/scala/eu/derfniw/aoc2024/day21/Day21.scala
@@ -2,113 +2,97 @@ package eu.derfniw.aoc2024.day21
 
 import eu.derfniw.aoc2024.util.{InputReader, runBenchmarked}
 
-type Route = String
-extension (r: Route)
-  def revertRoute: Route = r.reverse.collect {
-    case '^' => 'v'
-    case 'v' => '^'
-    case '<' => '>'
-    case '>' => '<'
-  }
+import scala.collection.mutable
 
-  def keyPadToDirPad: Route =
-    def _keyPadToDirPad(remainder: Route): Route =
-      if remainder.size <= 1 then ""
-      else
-          val (a, b) = (remainder.head, remainder.tail.head)
-          if a == b then "A" + _keyPadToDirPad(remainder.tail)
-          else KeyPad.routes((a, b)) + "A" + _keyPadToDirPad(remainder.tail)
-    _keyPadToDirPad("A" + r)
+case class Point(x: Int, y: Int)
 
-  def dirPadToDirPad: Route =
-    def _dirPadToDirPad(remainder: Route): Route =
-      if remainder.size <= 1 then ""
-      else
-          val (a, b) = (remainder.head, remainder.tail.head)
-          if a == b then "A" + _dirPadToDirPad(remainder.tail)
-          else DirPad.routes((a, b)) + "A" + _dirPadToDirPad(remainder.tail)
-    _dirPadToDirPad("A" + r)
-end extension
+class KeyPad(keypad: String):
+  private val keyMap: Map[Char, Point] = keypad
+    .split("\n")
+    .zipWithIndex
+    .flatMap { (line, y) =>
+      line.zipWithIndex.map { (c, x) =>
+        c -> Point(x, y)
+      }
+    }
+    .toMap
 
-object KeyPad:
-  // Predefined first half of the routes, this is small enough to manage by hand.
-  // Reference:
-  //   +---+---+---+
-  //   | 7 | 8 | 9 |
-  //   +---+---+---+
-  //   | 4 | 5 | 6 |
-  //   +---+---+---+
-  //   | 1 | 2 | 3 |
-  //   +---+---+---+
-  //       | 0 | A |
-  //       +---+---+
-  // format: off
-  private val baseRoutes: Map[(Char, Char), Route] = Map(
-    ('A', '0') -> "<",    ('A', '1') -> "^<<",  ('A', '2') -> "<^",   ('A', '3') -> "^",
-    ('A', '4') -> "<<^^", ('A', '5') -> "<^^",  ('A', '6') -> "^^",   ('A', '7') -> "^^^<<",
-    ('A', '8') -> "<^^^", ('A', '9') -> "^^^",
-    ('0', '1') -> "^<",   ('0', '2') -> "^",    ('0', '3') -> ">^",   ('0', '4') -> "^^<",
-    ('0', '5') -> "^^",   ('0', '6') -> ">^^",  ('0', '7') -> "^^^<", ('0', '8') -> "^^^",
-    ('0', '9') -> ">^^^",
-    ('1', '2') -> ">",    ('1', '3') -> ">>",   ('1', '4') -> "^",    ('1', '5') -> ">^",
-    ('1', '6') -> ">>^",  ('1', '7') -> "^^",   ('1', '8') -> ">^^",  ('1', '9') -> ">>^^",
-    ('2', '3') -> ">",    ('2', '4') -> "<^",   ('2', '5') -> "^",    ('2', '6') -> ">^",
-    ('2', '7') -> "<^^",  ('2', '8') -> "^^",   ('2', '9') -> ">^^",
-    ('3', '4') -> "<<^",  ('3', '5') -> "<^",   ('3', '6') -> "^",    ('3', '7') -> "<<^^",
-    ('3', '8') -> "<^^",  ('3', '9') -> "^^",
-    ('4', '5') -> ">",    ('4', '6') -> ">>",   ('4', '7') -> "^",    ('4', '8') -> "<^",
-    ('4', '9') -> ">>^",
-    ('5', '6') -> ">",    ('5', '7') -> "<^",   ('5', '8') -> "^",    ('5', '9') -> ">^",
-    ('6', '7') -> "<<^",  ('6', '8') -> "<^",   ('6', '9') -> "^",
-    ('7', '8') -> ">",    ('7', '9') -> ">>",
-    ('8', '9') -> ">"
-  )
-  // format: on
-  val routes: Map[(Char, Char), Route] = baseRoutes ++ baseRoutes.map { case (k, v) =>
-    (k.swap, v.revertRoute)
-  }
+  // Calculates the route in either "vertical -> horizontal" or "horizontal -> vertical"
+  // (more turns increases movements on successive layers of dirpads, so will not be optimal.).
+  // Takes into account the forbidden tile (empty space).
+  def routes(from: Char, to: Char): Set[List[Char]] =
+    val fromPoint = keyMap(from)
+    val toPoint   = keyMap(to)
+    val dx        = toPoint.x - fromPoint.x
+    val dy        = toPoint.y - fromPoint.y
 
+    val ySteps = List.fill(math.abs(dy))(if dy < 0 then '^' else 'v')
+    val xSteps = List.fill(math.abs(dx))(if dx < 0 then '<' else '>')
+    val result = mutable.Set.empty[List[Char]]
+    // Vertical is safe
+    if keyMap(' ') != Point(fromPoint.x, toPoint.y) then
+      result += ('A' +: xSteps ++: ySteps).reverse
+    // Horizontal is safe
+    if keyMap(' ') != Point(toPoint.x, fromPoint.y) then
+      result += ('A' +: ySteps ++: xSteps).reverse
+    result.toSet
+  end routes
 end KeyPad
 
-object DirPad:
-  // Predefined first half of the routes, this is small enough to manage by hand.
-  // Reference:
-  //   +---+---+---+
-  //   |   | ^ | A |
-  //   +---+---+---+
-  //   | < | v | > |
-  //   +---+---+---+
-  // format: off
-  private val baseRoutes: Map[(Char, Char), Route] = Map(
-    ('A', '^') -> "<",  ('A', 'v') -> "<v", ('A', '<') -> "v<<", ('A', '>') -> "v",
-    ('^', '<') -> "v<", ('^', 'v') -> "v",  ('^', '>') -> ">v",
-    ('>', '<') -> "<<", ('>', 'v') -> "<",
-    ('v', '<') -> "<",
+object KeyPad:
+  private val numericPad: KeyPad = KeyPad(
+    """|789
+       |456
+       |123
+       | 0A""".stripMargin
   )
-  // format: on
-  val routes: Map[(Char, Char), Route] = baseRoutes ++ baseRoutes.map { case (k, v) =>
-    (k.swap, v.revertRoute)
-  }
+  private val dirPad: KeyPad = KeyPad(
+    """| ^A
+       |<v>""".stripMargin
+  )
 
-end DirPad
+  def keyPadsForLayers(layers: Int): IndexedSeq[KeyPad] =
+    numericPad +: IndexedSeq.fill(layers)(dirPad)
+end KeyPad
 
-def part1(input: Seq[String]): Int =
-  input.map { in =>
-    val numeric = in.take(3)
-    val route = in.keyPadToDirPad.dirPadToDirPad.dirPadToDirPad
-    numeric.toInt * route.size
-  }.sum
+class Calculator(depth: Int):
+  private val keyPads = KeyPad.keyPadsForLayers(depth)
 
-def part2(input: Seq[String]): Int =
-  input.map { in =>
-    val numeric = in.take(3)
-    val route = in.keyPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad
-      .dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad
-      .dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad
-      .dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad
-      .dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad.dirPadToDirPad
-    numeric.toInt * route.size
-  }.sum
+  def getComplexity(input: List[Char]): Long =
+    input.take(3).mkString.toInt * minPressesNeeded(input, keyPads)
+
+  private def minPressesNeeded(code: List[Char], keyPads: IndexedSeq[KeyPad]): Long = keyPads match
+    case Nil => code.length
+    case kps =>
+      // Codes end with A (see input & route generation above)
+      // And we always start at A as well.
+      ('A' :: code)
+        .sliding(2)
+        .collect { case from :: to :: _ =>
+          minPressesNeededForMove(from, to, kps)
+        }
+        .sum
+  end minPressesNeeded
+
+  private val cache   = mutable.Map.empty[(Char, Char, Int), Long]
+  private def minPressesNeededForMove(currentKey: Char, nextKey: Char, keyPads: IndexedSeq[KeyPad]): Long =
+    cache.getOrElseUpdate(
+      (currentKey, nextKey, keyPads.length),
+      keyPads.head
+        .routes(currentKey, nextKey)
+        .map(route => minPressesNeeded(route, keyPads.tail))
+        .min
+    )
+  end minPressesNeededForMove
+
+
+def part1(input: Seq[String]): Long =
+  val calculator = Calculator(2)
+  input.map(_.toList).map(calculator.getComplexity).sum
+
+def part2(input: Seq[String]): Long =
+  val calculator = Calculator(25)
+  input.map(_.toList).map(calculator.getComplexity).sum
 
 object Input extends InputReader(21)
 
